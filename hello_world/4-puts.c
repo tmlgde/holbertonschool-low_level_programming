0a05b603cde83d2f08c0b1e0437d7b7b6c5d5b54
tmlgde <tom.lagarde33@outlook.fr>
2025-02-17 12:54:53 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
- *main: point d'entree du programme
- *return: 0 si tout se passe bien
+ * main - point d'entrée du programme
+ *
+ * Description: Ce programme affiche exactement la phrase suivante :
+ * puis retourne 0 pour indiquer que tout s'est bien passé.
+ *
+ * Return: 0 si tout se passe bien
  */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
