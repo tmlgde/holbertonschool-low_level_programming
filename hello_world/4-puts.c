52c724c96fe857dc4d4e91593950d693e1cf35cb
tmlgde <tom.lagarde33@outlook.fr>
2025-02-17 12:49:42 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ *main: point d'entree du programme
+ *return: 0 si tout se passe bien
+ */
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+Int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
