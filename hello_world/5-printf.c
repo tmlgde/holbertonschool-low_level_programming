423c71a28ea5238144c748f75003b08c0c74964b
tmlgde <tom.lagarde33@outlook.fr>
2025-02-17 13:14:40 +0000

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main: programme d'entree du programme
+ * printf: afficher le message entre parenthese
+ * return 0: si le code fonctionne
 int main(void)
 {
 	printf ("with proper grammar, but the outcome is a piece of art,\n");
