2055b8c4ae5f322415606d613c86172dd791208c
tmlgde <tom.lagarde33@outlook.fr>
2025-02-18 13:22:54 +0000

    afficher deux alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Prints a programming quote
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	char A;
+
+	for (a = 'a'; a <='z'; a++) 
+	{
+	       	putchar(a);
+	}
+	
+	for (A = 'A'; A <='Z'; A++)
+	{
+		putchar(A);
+	}
+	putchar('\n');
+
+	return (0);
+}	
+
+	
