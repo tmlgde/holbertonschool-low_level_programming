a1ec6540fab688b6f7ee2d7aa93b560a2c5734bc
tmlgde <tom.lagarde33@outlook.fr>
2025-02-18 08:56:22 +0000

    variable n avec plusieurs conditions

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if ( the last digit of n > 5 )
+	{
+		
+		printf ("%d greater than 5"\n, n)
+	}
+
+	if ( the last digit of n == 0 )
+	{
+		printf ("%d is 0"\n, n)
+	}
+
+	if ( the last digit of n > 6 and 0 )
+	{
+		printf("% is less than 6 and not 0")
+	}
+	return (0);
+}
