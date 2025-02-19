c16b2cd6ed24d48fb96db7aba0bb672a83dbf201
tmlgde <tom.lagarde33@outlook.fr>
2025-02-19 09:24:12 +0000

    imprimer chiffres en base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * main - Prints a programming quote
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	char b;
+
+	for (a = '0'; a <= '9'; a++)
+
+	{
+		putchar(a);
+	}
+
+	for (b = 'a'; b < 'g'; b++)
+
+	{
+		putchar (b);
+	}
+	putchar('\n');
+		return (0);
+}
