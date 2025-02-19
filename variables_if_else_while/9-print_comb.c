13a79a90149defcc726aa98186e333e1b7527688
tmlgde <tom.lagarde33@outlook.fr>
2025-02-19 10:19:24 +0000

    imprimer des chiffres avec espaces et virgules

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,10 +9,14 @@ int main(void)
 	int a;
 
 	for (a = '0'; a <= '9'; a++)
+
 	{
-		putchar(a);
-		putchar(',');
+		putchar('0' + a);
+		if (a < 9)
+		{
 		putchar(' ');
+		putchar (',');
+		}
 	}
 
 	putchar('\n');
