2d1c481a1ef1d5200b3c6a9973b0de46ef7a9fd8
tmlgde <tom.lagarde33@outlook.fr>
2025-02-19 14:06:23 +0000

    0. _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+/**
+ * main - Prints a programming quote
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(1, "_putchar\n", 9);
+
+	return (0);
+}
