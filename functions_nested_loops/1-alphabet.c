9113e0a9bb2718690de53873f3a0c3e9a43eaf88
tmlgde <tom.lagarde33@outlook.fr>
2025-02-20 14:27:20 +0000

    declarer et appeler une fonction pour imprimer l'alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_alphabet: imprimer l'alphabet grace a la fonction
+ * print_alphabet - Prints the lowercase alphabet followed by a new line.
  *
+ * Description:
+ * This function uses a loop to iterate through the lowercase alphabet,
+ * starting from 'a' to 'z', and prints each character using the _putchar
+ * function. After printing all the letters, it prints a newline character
+ * to move to the next line.
+ *
+ * No return value is expected as the function is of type void.
  */
 
 void print_alphabet(void)
