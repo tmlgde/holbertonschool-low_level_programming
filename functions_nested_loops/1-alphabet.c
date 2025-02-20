4506cb4d1ebc7fb40b8a75438f2f0d3e6bd592d8
tmlgde <tom.lagarde33@outlook.fr>
2025-02-20 14:12:32 +0000

    declarer et appeler une fonction pour imprimer l'alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+{
+	putchar(a);
+
+}
+putchar('\n');
+}
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+void print_alphabet(void);
+int _putchar(char c);
+
+#endif
