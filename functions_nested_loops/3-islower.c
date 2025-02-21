98653691ddf76d00835d4965d51ae58edebf9d33
tmlgde <tom.lagarde33@outlook.fr>
2025-02-21 09:27:17 +0000

    islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,15 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _islower - Vérifie si un caractère est en minuscule.
- * @c: Le caractère à vérifier.
+ * _islower - Checks if a character is lowercase.
+ * @c: The character to check.
  *
- * Description : Cette fonction prend un caractère en entrée et vérifie s'il
- * est compris entre 'a' et 'z' (inclus), ce qui signifie qu'il s'agit d'une
- * lettre minuscule. Si c'est le cas, la fonction retourne 1, sinon elle retourne 0.
- * 
- * Retourne : 1 si le caractère est une lettre minuscule, sinon 0.
+ * Description: This function takes a character as input and checks if it
+ * is a lowercase letter, meaning it is between 'a' and 'z' (inclusive).
+ * If the character is lowercase, the function returns 1. Otherwise, it
+ * returns 0.
+ *
+ * Return: 1 if the character is a lowercase letter, otherwise returns 0.
  */
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
