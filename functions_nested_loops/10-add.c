a9f9151569ffe037fb52d1a3b558e5a53f6c73be
tmlgde <tom.lagarde33@outlook.fr>
2025-02-21 15:01:18 +0000

    addition d'une variable

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - Adds two integers.
+ * @a: The first integer to be added.
+ * @b: The second integer to be added.
+ *
+ * Description: This function takes two integers as input, adds them together,
+ * and returns the sum. The result is returned as an integer.
+ *
+ * Return: The sum of the two integers (a + b).
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
