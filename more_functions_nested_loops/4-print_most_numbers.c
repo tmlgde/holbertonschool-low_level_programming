3e78cc13cb4dc7571d90a7fbd0f5e831af7b0e0e
tmlgde <tom.lagarde33@outlook.fr>
2025-02-24 10:44:50 +0000

    tous les chiffres sauf 2 et 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_most_numbers - Affiche les chiffres de 0 à 9, en excluant 2 et 4.
+ *
+ * Description: Cette fonction affiche les chiffres de 0 à 9, sauf les chiffres
+ * 2 et 4, suivis d'un saut de ligne. Elle utilise la fonction _putchar pour afficher
+ * chaque chiffre.
+ */
 void print_most_numbers(void)
 {
 	int a;
