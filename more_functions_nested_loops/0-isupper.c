2ec562a5c2199ddb9e1eebac6c23fc1aa4dd081a
tmlgde <tom.lagarde33@outlook.fr>
2025-02-24 08:56:38 +0000

    le read me

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-
+**
+ * _isupper - Vérifie si un caractère est une lettre majuscule.
+ * @c: Le caractère à tester.
+ *
+ * Description: Cette fonction prend un caractère en entrée et renvoie 1
+ * si le caractère est une lettre majuscule (en l'occurrence 'A'), sinon
+ * elle renvoie 0. Cette fonction se limite à vérifier seulement si le
+ * caractère est 'A'.
+ *
+ * Return: 1 si le caractère est 'A', sinon 0.
+ */
+ 
 int _isupper(int c)
 {
 	if( c == 'A')
