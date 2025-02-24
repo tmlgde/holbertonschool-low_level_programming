c3db0aafc3ff60c41664520e1fdb92b974dce11a
tmlgde <tom.lagarde33@outlook.fr>
2025-02-24 09:15:29 +0000

    isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -5,20 +5,18 @@
  * _isupper - vérifie si un caractère est une lettre majuscule.
  * @c: le caractère à tester.
  *
- * Retourne 1 si le caractère est une lettre majuscule, sinon retourne 0.
- * Cette fonction vérifie uniquement si le caractère est égal à 'A'.
+ * Retourne 1 si le caractère est une lettre majuscule, sinon retourne 0
  *
  * Return: 1 si le caractère est 'A', sinon 0.
  */
- 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
