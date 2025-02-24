79022d7aa14190d76c8cce6d3e8a109a6617c570
tmlgde <tom.lagarde33@outlook.fr>
2025-02-24 08:52:16 +0000

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+int _isupper(int c)
+{
+	if( c == 'A')
+	{	return(1);
+	}
+
+	else
+	{	return(0);
+	}
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+int _isupper(int c);
+
+#endif
