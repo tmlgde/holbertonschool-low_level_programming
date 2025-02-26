da885c3fe8181d7eb61828e83694c3fe28833580
tmlgde <tom.lagarde33@outlook.fr>
2025-02-26 13:39:57 +0000

    afficher une chaine de caractere en sens inverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Affiche une chaîne de caractères en sens inverse.
+ * @s: La chaîne de caractères à afficher en sens inverse.
+ *
+ * Description: Cette fonction prend une chaîne de caractères et l'affiche
+ * caractère par caractère en sens inverse. Elle utilise deux boucles 
+ * `for` pour parcourir la chaîne et l'afficher dans l'ordre inverse.
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	{
+		for (i--; i >= 0; i--)
+		{
+			_putchar(s[i]);
+		}
+		_putchar('\n');
+	}
+}
