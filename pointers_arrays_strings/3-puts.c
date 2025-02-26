1c6ec0cd76357c72a2711899a8a1636a48525d2d
tmlgde <tom.lagarde33@outlook.fr>
2025-02-26 13:06:53 +0000

    afficher une chaine de caracteres

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 
 #endif
