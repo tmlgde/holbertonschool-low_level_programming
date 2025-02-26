00753fcd571b895460519d93176907721c948c27
tmlgde <tom.lagarde33@outlook.fr>
2025-02-26 13:08:17 +0000

    afficher une chaine de caracteres

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * _puts - Affiche une chaîne de caractères suivie d'un saut de ligne.
+ * @str: Pointeur vers la chaîne de caractères à afficher.
+ *
+ * Description: Cette fonction prend une chaîne de caractères en paramètre
+ * et l'affiche caractère par caractère, puis ajoute un saut de ligne à la fin.
+ */
 void _puts(char *str)
 {
 	int i;
