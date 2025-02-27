4b6bcbd37c9002a5e4f934d0c5dc28a00bc7000f
tmlgde <tom.lagarde33@outlook.com>
2025-02-27 09:58:16 +0000

    afficher une variable

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * reset_to_98 - Modifie la valeur de l'entier pointé par n à 
+ * reset_to_98 - Modifie la valeur de l'entier pointé par 
  * @n: Pointeur vers l'entier dont la valeur doit être modifié
  *
  * Description: Cette fonction prend un pointeur vers un entier et modifie
- * sa valeur pour la mettre à 98. Si le pointeur est valide, la valeur 
+ * sa valeur pour la mettre à 98. Si le pointeur est valide, la valeu
  * l'entier pointé sera changé
  */
 void reset_to_98(int *n)
