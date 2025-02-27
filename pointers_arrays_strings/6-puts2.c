73ed1bbcce4cffaf3ecc348820b04dcc48e281cd
tmlgde <tom.lagarde33@outlook.com>
2025-02-27 13:18:39 +0000

    afficher deux par deux

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,19 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * puts2 - Affiche chaque deuxième caractère d'une chaîne de caractères.
+ * @str: Pointeur vers la chaîne de caractères à afficher.
+ *
+ * Cette fonction parcourt la chaîne et affiche les caractères situés
+ * à des indices pairs, puis imprime un saut de ligne à la fin.
+ */
+void puts2(char *str)
+{
+	int i;
 
-
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
