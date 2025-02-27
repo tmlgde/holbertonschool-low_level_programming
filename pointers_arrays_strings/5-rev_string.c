b1ecb8d2bad4dde3e80af3c3f54a6f1be1a322d7
tmlgde <tom.lagarde33@outlook.com>
2025-02-27 10:49:45 +0000

    afficher une chaien de caractere en inverser

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,18 +1,28 @@
 #include "main.h"
-#include <string.h>
-
+#include <stdio.h>
+/**
+ * rev_string - Fonction qui inverse une chaîne de caractères.
+ * @s: Pointeur vers la chaîne de caractères à inverser.
+ *
+ * Cette fonction modifie la chaîne en place sans utiliser de mémoire supplémentaire.
+ */
 void rev_string(char *s)
 {
-	int i;
-	int j = strlen(s) - 1;
+	int i = 0;
+	int j;
 
-	while (i < j)
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	j = i - 1;
+
+	for (i = 0; i < j; j--)
 	{
 		char temp = s[i];
-	s[i] = s[j];
-	s[j] = temp;
-
-	i++;
-	j--;
-}
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+	}
 }
