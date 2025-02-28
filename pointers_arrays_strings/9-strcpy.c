ac1d19eb87d5df325d8746f388f588f977c44247
tmlgde <tom.lagarde33@outlook.fr>
2025-02-28 12:39:13 +0000

    copier une chaine de caracteres vers un pointeur

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,17 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strcpy - Copie la chaîne de caractères pointée par src vers dest.
- * @dest: Pointeur vers le buffer de destination où la chaîne sera copiée.
- * @src: Pointeur vers la chaîne source à copier.
+ * _strcpy - Copie la chaîne de src vers dest.
+ * @dest: Pointeur vers le buffer de destination.
+ * @src: Pointeur vers la chaîne source.
  *
- * Cette fonction copie les caractères de la chaîne src dans la chaîne dest,
- * y compris le caractère de fin de chaîne '\0'. Elle retournn pointeurve
- * la chaîne destination (dest).
- *
- * Retourne: Le pointeur vers dest, qui contient la chaîne copiée.
- * Cela permet à l'appelant de continuer à utiliser ou manipuler la chaîne
- * copiée dans dest après l'appel de la fonction.
+ * Return: Le pointeur vers dest.
  */
 char *_strcpy(char *dest, char *src)
 {
