1af05757a5c1fb2e80909adc57026d73baa25779
tmlgde <tom.lagarde33@outlook.fr>
2025-03-04 12:43:17 +0000

    localiser une sous chaine dans une chaine donnee

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - Locates a substring in a string.
+ * @haystack: The string to search.
+ * @needle: The substring to find.
+ *
+ * Return: A pointer to the first occurrence of needle in haystack,
+ *         or NULL if needle is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+    int i, j;
+
+    if (!needle[0])
+        return haystack;
+
+    for (i = 0; haystack[i] != '\0'; i++)
+    {
+        for (j = 0; needle[j] != '\0'; j++)
+        {
+           if (haystack[i + j] != needle[j])
+                break;
+        }
+
+            if (needle[j] == '\0')
+            return (haystack + i);
+    }
+
+    return NULL;
+}
