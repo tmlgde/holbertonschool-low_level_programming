b695206f9695885ce10be3f13032024dd2c396b3
tmlgde <tom.lagarde33@outlook.fr>
2025-03-04 08:45:45 +0000

    exo 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,16 +7,21 @@
  * Return: Always 0.
  */
 
- char *_strchr(char *s, char c)
-    {
-        int i = 0;
-        while (s[i] != '\0')
-        {
-            if (s[i] == c)
-            {
-                return (s + i);
-            }
-            i++;
-        }
-        return (NULL);
-    }
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		if (c == '\0')
+		{
+		return (s + i);
+		}
+		i++;
+	}
+	return (NULL);
+	}
