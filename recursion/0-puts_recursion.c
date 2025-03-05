4bdc020b885f6d113f0e0da6501c95914c12c696
tmlgde <tom.lagarde33@outlook.fr>
2025-03-05 10:22:43 +0000

    imprimer une ligne avec recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * _puts_recursion - Prints a string followed by a new line.
+ * @s: String to print.
+ */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
