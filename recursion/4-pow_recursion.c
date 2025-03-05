ad35b33329ff0983e2afeeb86232c8804bfca0b2
tmlgde <tom.lagarde33@outlook.fr>
2025-03-05 13:51:44 +0000

    calculer a la puissance grace a la recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -10,10 +10,10 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	{
-		return (-1)
+		return (-1);
 	}
 
-	if (y = 0)
+	if (y == 0)
 	{
 		return (1);
 	}
@@ -22,4 +22,5 @@ int _pow_recursion(int x, int y)
 	{
 		return (x * _pow_recursion(x, y - 1));
 	}
+	return (x);
 }
