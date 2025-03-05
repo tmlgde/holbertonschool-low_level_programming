ab7b6ca18e439e2136c71d30101bb9ae520a916e
tmlgde <tom.lagarde33@outlook.fr>
2025-03-05 10:35:12 +0000

    afficher une chaine inversee

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion - Prints a string in reverse, followed by a new line.
+ * @s: String to print in reverse.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
