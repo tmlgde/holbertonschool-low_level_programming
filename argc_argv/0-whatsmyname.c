61eb51cc1877247f2a22e65eaf1042a962562fa2
tmlgde <you@example.com>
2025-03-06 14:12:56 +0000

    prints its name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Affiche le nom du programme
+ * @argc: Nombre d'arguments (non utilisé)
+ * @argv: Tableau des arguments
+ *
+ * Retour: 0 si succès
+ */
 int main(int argc, char *argv[])
 {
 	(void) argc;
