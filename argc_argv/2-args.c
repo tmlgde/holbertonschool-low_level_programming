2a8af87db89438490f925d9ae23019253bb24ce3
tmlgde <tom.lagarde33@outlook.fr>
2025-03-07 12:50:31 +0000

    afficher ligne par ligne

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+/**
+ * main - Prints all command-line arguments
+ * @argc: Argument count
+ * @argv: Argument vector (array of strings)
+ *
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
 	int a;
 
 	for (a = 0; a < argc; a++)
-	printf("%s\n",argv[a]);
+	printf("%s\n", argv[a]);
 	return (0);
 }
