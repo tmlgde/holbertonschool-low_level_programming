bd20510b86d4fd29e977d1fb9ed07df69fe8e22c
tmlgde <tom.lagarde33@outlook.fr>
2025-03-12 11:32:07 +0000

    str_concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -44,6 +44,10 @@ char *str_concat(char *s1, char *s2)
 	}
 
 	nvlchaine = malloc(_strlen(s1) + _strlen(s2) + 1);
+	if (nvlchaine == 0 );
+	{
+		return (NULL);
+	}
 
 	for (i = 0; s1[i] != '\0'; i++)
 	{
