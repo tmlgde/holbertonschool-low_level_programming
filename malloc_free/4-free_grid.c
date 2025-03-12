7dc3fac91d52473ec57e219af9dc0fb6d7f9ad87
tmlgde <tom.lagarde33@outlook.fr>
2025-03-12 11:47:25 +0000

    free

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -6,20 +6,9 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == 0)
-	{
-		return;
-	}
-
-	if (height == 0)
-	{
-		return;
-	}
-
 	for (i = 0; i < height ; i++)
 	{
 		free(grid[i]);
 	}
 	free(grid);
 }
-
