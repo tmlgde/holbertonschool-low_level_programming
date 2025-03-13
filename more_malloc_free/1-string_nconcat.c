8b213e3469918bfc9ee1a0fd21f7879ec286fdb8
tmlgde <tom.lagarde33@outlook.fr>
2025-03-13 11:53:44 +0000

    concatener deux chaines en prenant seulement une partie de la deuxieme chaine

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * string_nconcat - Concatenates two strings up to n bytes from s2.
+ * @s1: First string.
+ * @s2: Second string.
+ * @n: Number of bytes to concatenate from s2.
+ *
+ * Return: Pointer to the concatenated string or NULL.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int lg1 = 0, lg2 = 0, i, j;
+	char *resultat;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[lg1])
+		lg1++;
+
+	while (s2[lg2])
+		lg2++;
+
+	if (n > lg2)
+		n = lg2;
+
+	resultat = malloc((lg1 + n + 1) * sizeof(char));
+	if (resultat == NULL)
+		return (NULL);
+
+	for (i = 0; i < lg1; i++)
+		resultat[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		resultat[i + j] = s2[j];
+
+	resultat[i + j] = '\0';
+
+	return (resultat);
+}	
