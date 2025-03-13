49e7765f9a7aa40c962123eff12ebb577cdd7922
tmlgde <tom.lagarde33@outlook.fr>
2025-03-13 13:26:01 +0000

    allouer de la memoire pour un tableau

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,7 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
-
+/**
+ * _calloc - Alloue de la mémoire pour un tableau d'éléments et initialise à 0.
+ * @nmemb: Nombre d'éléments.
+ * @size: Taille de chaque élément.
+ *
+ * Return: Pointeur vers la mémoire allouée, ou NULL en cas d'échec.
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *resultat;
