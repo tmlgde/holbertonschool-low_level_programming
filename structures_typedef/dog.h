8f69f78190f8fa39e72271f5b8a0bc89cdaf9737
tmlgde <tom.lagarde33@outlook.fr>
2025-03-13 14:53:05 +0000

    structure de base

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
+/**
+ * struct dog - A dog's basic information
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
 struct dog
 {
 	char *name;
