8ce80349bcf37b0a4a92d7a4942b6b60a58d0283
tmlgde <tom.lagarde33@outlook.fr>
2025-03-13 09:57:40 +0000

    TT

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * malloc_checked - Alloue de la mémoire et vérifie si l'allocation réussit.
+ * @b: Nombre d'octets à allouer.
+ *
+ * La fonction alloue `b` octets avec `malloc`. Si l'allocation échoue,
+ * le programme se termine avec le code d'erreur 98.
+ *
+ * Retour : Pointeur vers la mémoire allouée ou exit(98) en cas d'échec.
+ */
 void *malloc_checked(unsigned int b)
 {
 	char *gigi;
