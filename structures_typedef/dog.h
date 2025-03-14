bdd687967af2f9d847387c84bac91bef7e0ca766
tmlgde <tom.lagarde33@outlook.fr>
2025-03-14 22:47:42 +0000

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,20 +1,26 @@
-#ifndef MYHEADER_H
-#define MYHEADER_H
+#ifndef DOG_H
+#define DOG_H
+
 /**
- * struct dog - A dog's basic information
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
+ * struct dog - Structure that stores information about a dog
+ * @name: Name of the dog
+ * @age: Age of the dog in years
+ * @owner: Name of the dog's owner
+ *
+ * This structure stores information about a dog, including its name,
+ * age, and owner's name.
  */
-struct dog
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
