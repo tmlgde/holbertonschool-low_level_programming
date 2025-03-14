d14de1235f2c80279bfc16de19b70c123bc676ff
tmlgde <tom.lagarde33@outlook.fr>
2025-03-14 13:03:22 +0000

    variable de type struct dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stddef.h>
+/**
+ * init_dog - initialise une variable de type struct dog
+ * @d: Pointeur vers la structure dog
+ * @name: Nom du chien
+ * @age: Âge du chien
+ * @owner: Propriétaire du chien
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    if (d != NULL)
+    {
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+    }
+}
