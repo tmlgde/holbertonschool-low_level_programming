912fec5be5155719c7f890ef3a2952300280baf2
tmlgde <tom.lagarde33@outlook.fr>
2025-03-19 12:01:30 +0000

    empty

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "calc.h"
+#include <stdio.h>
+/**
+ * op_add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b)
+}
+
+/**
+ * op_add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b)
+}
+/**
+ * op_add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The mul of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b)
+}
+/**
+ * op_add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The division of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b)
+}
+/**
+ * op_add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The modulo of a and b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b)
+}
+
