c44c7d758e5276a3591082ab40c7d6996396cf98
tmlgde <tom.lagarde33@outlook.fr>
2025-03-20 13:18:56 +0000

    print numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -24,7 +24,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (separator != NULL && i < n - 1)
 		printf("%s", separator);
 	}
-	va_end(arguments);
 	printf("\n");
+	va_end(arguments);
 
 }
