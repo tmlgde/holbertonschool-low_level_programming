b0f87e1defbde5e29bc26167c38d1aaff5318199
tmlgde <tom.lagarde33@outlook.fr>
2025-03-20 13:53:17 +0000

    imprimer une chaine

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arguments;
+	char *str;
+
+	va_start(arguments, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(arguments, char *);
+
+		if (str != NULL)
+			printf("%s", str);
+		else
+			printf("(nil)");
+
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(arguments);
+                printf("\n");
+}
+
