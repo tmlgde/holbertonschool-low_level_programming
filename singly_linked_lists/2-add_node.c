55f4e3416f6b903baac3a740a5ab8bf7e2945d20
tmlgde <tom.lagarde33@outlook.fr>
2025-04-01 13:03:19 +0000

    rajouter un element puis l'ajouter en tete de liste

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,43 +1,34 @@
 #include "lists.h"
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * add_node - Adds a new node at the beginning of the list.
- * @head: Pointer to the first element in the list.
- * @str: String to be added to the new node.
+ * add_node - adds a new node at the beginning of a list.
+ * @head: head of the list.
+ * @str: pointer to the string.
  *
- * Return: The address of the new element, or NULL if it failed.
+ * Return: adress of the new element or NULL if it failed.
  */
-
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *newnode;
+	unsigned int len = 0;
 
-	new_node = malloc(sizeof(list_t));
+	newnode = malloc(sizeof(list_t));
 
-	if (!new_node)
-	{
+	if (newnode == NULL)
 		return (NULL);
-	}
 
-	if (!str)
-	{
-		return (NULL);
-	}
+	for (; str[len]; len++)
+		;
+	newnode->str = strdup(str);
+	newnode->len = len;
+	newnode->next = (*head);
+	*head = newnode;
 
-	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	return (newnode);
 
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = len;
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
 }
