605aea3224afbd50a811368a7ce70b98d296a66a
tmlgde <tom.lagarde33@outlook.fr>
2025-04-01 09:34:28 +0000

    afficher une liste chainee

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,19 +1,25 @@
-#include "main.h"
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
+	{
 
-		if (h->str = NULL)
+		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
-			printf("[%d] %s\n", h->len h->str);
-
-	h = h->next;
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 	count++;
+	h = h->next;
+	}
 	return (count);
 }	
--- singly_linked_lists/main.h
+++
@@ -1,4 +0,0 @@
-#ifndef MYHEADER_H
-#define MYHEADER_H
-size_t print_list(const list_t *h);
-#endif
