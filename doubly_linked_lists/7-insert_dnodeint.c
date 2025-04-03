a9850d65dd12bf8f6beac0d77e3a1ef413798c06
tmlgde <tom.lagarde33@outlook.fr>
2025-04-03 19:12:34 +0000

    ajouter un noeud a une list donnee

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
-
+/**
+ * insert_dnodeint_at_index - Inserts a node at a given index.
+ * @h: Pointer to the head of the list.
+ * @idx: Index where the node should be inserted.
+ * @n: Value of the new node.
+ * 
+ * Return: Address of the new node, or NULL on failure.
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *newnode, *vartemp = *h;
