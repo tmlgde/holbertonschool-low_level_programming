fc8b0bf2ee39a12af32ed64668b8cbcf9636f42b
tmlgde <tom.lagarde33@outlook.fr>
2025-04-03 19:17:38 +0000

    ajouter un noeud a une list donnee

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,47 +1,42 @@
 #include "lists.h"
+
 /**
- * insert_dnodeint_at_index - Inserts a node at a given index.
- * @h: Pointer to the head of the list.
- * @idx: Index where the node should be inserted.
- * @n: Value of the new node
- * Return: Address of the new node, or NULL on failure.
+ * insert_dnodeint_at_index - Inserts a new node at a given position
+ * @h: Pointer to the head of the list
+ * @idx: Index where to insert
+ * @n: Value to store in the new node
+ *
+ * Return: Address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newnode, *vartemp = *h;
+	dlistint_t *tmp = *h, *new_node;
 	unsigned int i = 0;
 
-	newnode = malloc(sizeof(dlistint_t));
-
-	if (newnode == NULL)
-	{
-		return (NULL);
-	}
-
 	if (idx == 0)
-	{
 		return (add_dnodeint(h, n));
-	}
 
-	if (!vartemp->next)
+	while (tmp != NULL && i < idx - 1)
 	{
-		return (add_dnodeint_end(h, n));
-	}
-
-	if (!vartemp)
-	{
-		return (NULL);
-	}
-
-	while (i < idx - 1 && vartemp != NULL)
-	{
-		vartemp = vartemp->next;
+		tmp = tmp->next;
 		i++;
 	}
 
-	newnode->next = vartemp->next;
-	newnode->prev = vartemp;
-	vartemp->next->prev = newnode;
-	vartemp->next = newnode;
-	return (newnode);
+	if (tmp == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = tmp;
+	new_node->next = tmp->next;
+
+	if (tmp->next != NULL)
+		tmp->next->prev = new_node;
+
+	tmp->next = new_node;
+
+	return (new_node);
 }
