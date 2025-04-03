e0581d4a87a509b117a1c127dff2f447b5306044
tmlgde <tom.lagarde33@outlook.fr>
2025-04-03 09:41:45 +0000

    liberer une double liste chainee

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdlib.h>
-
+/**
+ * free_dlistint - Libère une liste doublement chaînée
+ * @head: Pointeur vers le premier nœud de la liste
+ *
+ * Description: Cette fonction parcourt la liste chaînée et libère
+ *              chaque nœud un par un pour éviter les fuites de mémoire.
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *vartemp;
