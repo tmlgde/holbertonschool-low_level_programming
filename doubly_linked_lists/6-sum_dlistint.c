f2f3dec5cbb15feddc917adad6bfb5a08a56e9b6
tmlgde <tom.lagarde33@outlook.fr>
2025-04-03 13:19:29 +0000

    la somme de tous les noeuds

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * sum_dlistint - Sums all the data (n) in a doubly linked list.
+ * @head: Pointer to the first node.
+ * Return: Sum of all data, or 0 if the list is empty.
+ */
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *vartemp = head;
