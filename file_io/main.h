6ffbcce442ebb741b7437a333b21cff09577ba74
tmlgde <tom.lagarde33@outlook.fr>
2025-04-04 13:30:26 +0000

    lire et afficher un texte

--- file_io/main.h
+++ file_io/main.h
@@ -1,1 +1,14 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
