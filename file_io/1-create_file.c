8cda56de2a66e89985714fa34af583b90fec20d4
tmlgde <tom.lagarde33@outlook.fr>
2025-04-04 14:36:23 +0000

    creer un fichier

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,13 +19,13 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 	{
-		return(-1);
+		return (-1);
 	}
 
 	if (text_content == NULL)
 	{
 		close(fd);
-		return(1);
+		return (1);
 	}
 
 	while (text_content[len] != '\0')
@@ -34,9 +34,9 @@ int create_file(const char *filename, char *text_content)
 	if (write(fd, text_content, len) == -1)
 	{
 		close(fd);
-		return(-1);
+		return (-1);
 	}
 
 	close(fd);
-	return(1);
+	return (1);
 }
