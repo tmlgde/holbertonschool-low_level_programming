2f40c862cef8fe24d3e01c025deb32be4851a02d
tmlgde <tom.lagarde33@outlook.fr>
2025-04-04 13:27:21 +0000

    lire et afficher un texte

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -20,12 +20,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	fd = open(filename, O_RDONLY);
-		if (fd == - 1)
+		if (fd == -1)
 		{
 			return (0);
 		}
-
-
 	buffer = malloc(letters);
 	if (buffer == NULL)
 	{
@@ -34,25 +32,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	letsread = read(fd, buffer, letters);
-
-	if (letsread == - 1)
+	if (letsread == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	letswrite = write(STDOUT_FILENO, buffer, letsread);
+	if (letswrite == -1 || letswrite != letsread)
 	{
 		free(buffer);
 		close(fd);
 		return (0);
 	}
 
-	letswrite = write(STDOUT_FILENO, buffer, letsread);
-
-	if (letswrite == -1 || letswrite != letsread)
-	{
-		free(buffer);
-		close(fd);
-		return(0);
-	}
-
 	free(buffer);
 	close(fd);
-	return(letsread);
+	return (letsread);
 }
 
